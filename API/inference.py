2c466d1f97d75123d884ba00c022f05f18255ea4
David Tseng <87758475+bbOwOdd@users.noreply.github.com>
2025-06-26 16:32:11 +0800

    Add files via upload

---
+++ API/inference.py
@@ -0,0 +1,86 @@
+import nemo_run as run
+from nemo import lightning as nl
+from nemo.collections import llm
+import pytorch_lightning as pl
+from pathlib import Path
+from nemo.collections.llm.recipes.precision.mixed_precision import bf16_mixed
+from megatron.core.inference.common_inference_params import CommonInferenceParams
+
+peft_ckpt_path = str(next((d for d in Path("./results/nemo2_peft/checkpoints/").iterdir() if d.is_dir() and d.name.endswith("-last")), None))
+
+def example_dataset() -> run.Config[pl.LightningDataModule]:   
+                                         #, seq_length=4096, micro_batch_size=1, global_batch_size=8, num_workers=1)
+    return run.Config(llm.AlpacaDataModule, seq_length=2048, micro_batch_size=1, global_batch_size=2)
+
+def configure_inference(): #generate prediction data by the latest nemo checkpoint
+    return run.Partial(
+        llm.generate,
+        path=str(peft_ckpt_path),
+        trainer=trainer(),
+        input_dataset=example_dataset(),
+        inference_params=CommonInferenceParams(num_tokens_to_generate=20, top_k=0.8),
+        output_path="inference/peft_prediction.jsonl",
+    )
+
+def local_executor_torchrun(nodes: int = 1, devices: int = 2) -> run.LocalExecutor: #device: number of GPU
+    # Env vars for jobs are configured here
+    env_vars = {
+        "TORCH_NCCL_AVOID_RECORD_STREAMS": "1",
+        "NCCL_NVLS_ENABLE": "0",
+        "NVTE_DP_AMAX_REDUCE_INTERVAL": "0",
+        "NVTE_ASYNC_AMAX_REDUCTION": "1",
+        "TOKENIZERS_PARALLELISM": "False"
+    }
+
+    executor = run.LocalExecutor(ntasks_per_node=devices, launcher="torchrun", env_vars=env_vars)
+
+    return executor
+
+def trainer() -> run.Config[nl.Trainer]:
+    strategy = run.Config(
+        nl.MegatronStrategy,
+        tensor_model_parallel_size=2
+    )
+    trainer = run.Config(
+        nl.Trainer,
+        devices=2,
+        max_steps=143,
+        accelerator="gpu",
+        strategy=strategy,
+        plugins=bf16_mixed(),
+        log_every_n_steps=1,
+        limit_val_batches=2,
+        val_check_interval=2,
+        num_sanity_val_steps=0,
+    )
+    return trainer
+
+def logger() -> run.Config[nl.NeMoLogger]:
+    ckpt = run.Config(
+        nl.ModelCheckpoint,
+        save_last=True,
+        every_n_train_steps=10,
+        monitor="reduced_train_loss",
+        save_top_k=1,
+        save_on_train_epoch_end=True,
+        save_optim_on_train_end=True,
+    )
+
+    return run.Config(
+        nl.NeMoLogger,
+        name="nemo2_peft",
+        log_dir="./results",
+        use_datetime_version=False,
+        ckpt=ckpt,
+        wandb=None
+    )
+
+
+def llama3_8b() -> run.Config[pl.LightningModule]:
+    return run.Config(llm.LlamaModel, config=run.Config(llm.Llama3Config8B))
+
+if __name__ == "__main__":
+    # use the finetuned model to inference
+    run.run(configure_inference(), executor=local_executor_torchrun())
+    print("PEFT checkpoint path:", peft_ckpt_path)
+
